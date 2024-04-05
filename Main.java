7bcd8c4ba0f8326c226fc0e565e86d03c6bf7775
I-am-Sydney <165817702+I-am-Sydney@users.noreply.github.com>
2024-04-05 08:54:14 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,19 +1,42 @@
 import java.util.*;
-public class Main {
-    public static void main(String[] args){
-  
-     HashMap<Character, String> jjkCharacters = new HashMap<>();
-  jjkCharacters.put('G', "Gojo- Also known as The Honored One");
-  jjkCharacters.put('T', "Toji- Also known as the one who gave it all up");
-  jjkCharacters.put('S', "Sukuna- Also known as the King of Curses");
-  jjkCharacters.put('Y', "Yuta- Also known as The cursed child ");
+class Main{
+  public static Scanner input= new Scanner(System.in);
+  public static void main(String[] args){
+    while(true){
+      System.out.println("Would you like a show quote from:");
+      System.out.println("1- Spongebob");
+      System.out.println("2- The office");
+      System.out.println("3- My fairly odd parents");
+      int show= input.nextInt();
+      switch(show){
+        case 1:
+          spongebob();
+          break;
+        case 2:
+          dwight();
+          break;
+        case 3:
+          timmy();
+          break;
+          default:
+          System.out.println("Invalid choice");
+          input.next();
+      }
+      }
+    }
 
-Scanner  scanner = new Scanner(System.in);
-System.out.println("Enter a character to find out their name in Jujutsu Kaisen: ");
-char character = scanner.next().charAt(0);
-      String name=jjkCharacters.get (character);
-if (name == null){
-  }else{
-    System.out.println("I cant find the name of the character you entered");
-  }
+public static void spongebob(){
+  System.out.println("Is mayonnaise an instrument?");
+  input.nextLine();
+  input.nextLine();
+}
+public static void dwight(){
+  System.out.println("Would an idiot do that?");
+  input.nextLine();
+  input.nextLine();
+}
+public static void timmy(){
+  System.out.println("This year, Timmy Turner is not going to take it!");
+  input.nextLine();
+  input.nextLine();
 }}
